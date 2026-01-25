b48901407250b144ec435b01b4d62caf77bf14ed
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-01-25 16:17:30 -0600

    Metodo de buscar por metodo

--- src/main/java/com/tienda/api_tienda/repository/MetodoRepo.java
+++ src/main/java/com/tienda/api_tienda/repository/MetodoRepo.java
@@ -1,9 +1,12 @@
 package com.tienda.api_tienda.repository;
 
 import com.tienda.api_tienda.model.Metodo;
+import com.tienda.api_tienda.utils.MetodoEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MetodoRepo extends JpaRepository<Metodo, Long> {
+
+    boolean existsByNombre(MetodoEnum metodoEnum);
 }
